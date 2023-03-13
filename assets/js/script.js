0d14074f98d2cd23aa5c0996d6afff42838139f9
cgonz <christoph.gonz@gmail.com>
2023-03-13 14:41:59 -0700

    cleaned up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,18 +140,3 @@ function createPassword(lower, upper, num, spec, pwl) {
   //returns the password generated from the for loop
   return password;
 }
-
-/* code borrowed from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-
- function makeid(length) {
-   let result = "";
-   const characters =
-     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-   const charactersLength = characters.length;
-   let counter = 0;
-   while (counter < length) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-     counter += 1;
-   }
-   return result;
- } */
